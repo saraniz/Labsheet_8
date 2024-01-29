79313816a40e2a48a1e47e5374b5335863d41233
saraniz <158010091+saraniz@users.noreply.github.com>
2024-01-29 20:23:21 +0530

    Add files via upload

---
+++ pkg08/Labsheet08.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labsheet.pkg08;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author amand
+ */
+public class Labsheet08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       JFrame frame = new JFrame();
+       frame.setTitle("Temperature Converotr");
+       frame.setSize(500,500);
+       frame.setVisible(true);
+       
+    }
+    
+}
